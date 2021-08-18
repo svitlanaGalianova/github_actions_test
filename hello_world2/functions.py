fe3d36db74034e802482c62814fe7edc9880dfc3
Svitlana Galianova <Svitlana_Galianova@manulife.com>
2021-08-18 16:43:30 -0400

    test.py

--- hello_world2/functions.py
+++ hello_world2/functions.py
@@ -1,2 +1,6 @@
+import os
+secret = os.environ.get('SECRET_TEST')
+
+
 def hello_world2():
-    return "Hello World2!"
+    return secret
---
+++ test.py
@@ -0,0 +1,4 @@
+import os
+
+secret = os.environ.get('SECRET_TEST')
+print(secret)
